6617a32dd1993077f4dc4915f4ec6b3ebf2bb04d
joko triyanto <jokotriyanto123@outlook.com>
2021-08-22 20:50:05 +0700

    Finished Contact Page and Categories filter

--- src/components/Cart/CourseCart.js
+++ src/components/Cart/CourseCart.js
@@ -2,12 +2,39 @@ import React, { Component } from "react"
 import Heading from "../Reuseable/Heading"
 import Img from "gatsby-image"
 
+const getCaty = items => {
+  let holdItems = items.map(item => {
+    return item.node.category
+  })
+
+  let holdCategories = new Set(holdItems)
+  let categories = Array.from(holdCategories)
+  categories = ["all", ...categories]
+  return categories
+}
+
 export default class CourseCart extends Component {
   constructor(props) {
     super(props)
     this.state = {
       courses: props.courses.edges,
       mycourses: props.courses.edges,
+      mycategories: getCaty(props.courses.edges),
+    }
+  }
+
+  catyClicked = category => {
+    let keepItSafe = [...this.state.courses]
+
+    if (category === "all") {
+      this.setState(() => {
+        return { mycourses: keepItSafe }
+      })
+    } else {
+      let holdme = keepItSafe.filter(({ node }) => node.category === category)
+      this.setState(() => {
+        return { mycourses: holdme }
+      })
     }
   }
 
@@ -16,8 +43,26 @@ export default class CourseCart extends Component {
       <section className="py-5">
         <div className="container">
           <Heading title="Courses" textColor="text-black" />
+          <div className="row my-3">
+            <div className="col-10 mx-auto text-center">
+              {this.state.mycategories.map((category, index) => {
+                return (
+                  <button
+                    type="button"
+                    className="btn btn-info m-3 px-3"
+                    key={index}
+                    onClick={() => {
+                      this.catyClicked(category)
+                    }}
+                  >
+                    {category}
+                  </button>
+                )
+              })}
+            </div>
+          </div>
           <div className="row">
-            {this.state.courses.map(({ node }) => {
+            {this.state.mycourses.map(({ node }) => {
               return (
                 <div
                   key={node.id}
